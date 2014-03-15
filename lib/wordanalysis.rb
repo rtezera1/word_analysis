851c031ccf265f5846e29c864f50be7617ca94b2
Robel <robeltadesse01@gmail.com>
2014-03-15 16:19:48 -0400

    met all the pre-req

--- lib/wordanalysis.rb
+++ lib/wordanalysis.rb
@@ -37,5 +37,41 @@ class WordAnalysis
     @sorted_three_hash[@sorted_hash[-3][0]] = @sorted_hash[-3][1]
     @sorted_three_hash
   end
+
+  def top_three_common_letters
+    @common_letters = @sentence.split(%r{\s*})
+    @letters = @common_letters.inject (Hash.new(0)) { |letter,frequency| letter[frequency] +=1; letter}
+     @sorted_letters_hash = @letters.sort_by {|letter, frequency| -frequency}
+    @sorted_letter_three_hash = {}
+    @sorted_letter_three_hash[@sorted_letters_hash[0][0]] = @sorted_letters_hash[0][1]
+    @sorted_letter_three_hash[@sorted_letters_hash[1][0]] = @sorted_letters_hash[1][1]
+    @sorted_letter_three_hash[@sorted_letters_hash[2][0]] = @sorted_letters_hash[2][1]
+    @sorted_letter_three_hash
+  end
+
+  def number_of_words_only_used_once
+    @words = @sentence.split()
+    @words_hash = @words.inject (Hash.new(0)) { |word,frequency| word[frequency] +=1; word}
+    @used_once_words = @words_hash.select { |word, frequency| frequency < 2}
+      @used_once_words. each do |word, frequency|
+        frequency+=1
+        return frequency
+      end
+  end
+
+  def letters_not_used_in_sentence
+    @letters = @sentence.split(%r{\s*})
+    @alphabet = ("a".."z").to_a
+
+    @not_included_letters = []
+    @letters.each do |letter|
+      @alphabet.each do |one_letter|
+        if letter != one_letter
+          @not_included_letters<<one_letter
+        end
+      end
+    end
+    @not_included_letters
+  end
 end
 
--- spec/wordanalysis_spec.rb
+++ spec/wordanalysis_spec.rb
@@ -34,6 +34,22 @@ describe WordAnalysis do
   describe "top_three_common_letters" do
     it "selects the top three letters within a sentence" do
       setence_with_repetetive_letters = WordAnalysis.new("llll bbb cc a smith mike")
-      expect()
+      expect(setence_with_repetetive_letters.top_three_common_letters).to eq("l" => 4, "b" => 3, "c" => 2)
+    end
+  end
 
+  describe "number_of_words_only_used_once" do
+    it "selects the number of words only used once" do
+      setence_with_repetetive_words = WordAnalysis.new("is mike really the real mike or is he fake mike is the mike")
+      expect(setence_with_repetetive_words.number_of_words_only_used_once).to eq(5)
+    end
+  end
+
+  describe "letters_not_used_in_sentence" do
+    it "gives the letters not used in a sentence" do
+      words_used = WordAnalysis.new("mike szeth")
+      expect(words_used.letters_not_used_in_sentence).to eq(17)
+      binding.pry
+    end
+  end
 end
